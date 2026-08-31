30575391df27b84c497f5c80eb5f0aa2bc9f7365
Abhinab <shresthaabhinab111@gmail.com>
2026-08-31 15:38:26 +0545

    Product

--- Product.cpp
+++ Product.cpp
@@ -1,59 +1,46 @@
 #include<iostream>
 #include<string>
 using namespace std;
-class Product{
-    protected:
-    string productName,productCost,productType;
 
+class Products{
+    protected:
+    int id;
+    string name;
+    float price;
     public:
-    Product(){}
-    Product(string n , string c , string t){
-        productName = n;
-        productCost = c;
-        productType = t;
+    Products(int i , string n , float p){
+        id = i;
+        name = n;
+        price = p;
+    }
+
+    virtual void show(){
+        cout<<"Id : "<<id<<endl;
+        cout<<"Name : "<<name<<endl;
+        cout<<"Price : "<<price<<endl;
     }
 };
 
-int main(){
-    cout<<"   | Product                           | Cost (USD) | Type     |"<<endl;
-    cout<<"   | --------------------------------- | ---------- | -------- |"<<endl;
-    cout<<" 1 | Microsoft Office 365 Subscription | $70/year   | Digital  |"<<endl;
-    cout<<" 2 | Adobe Photoshop License           | $23/month  | Digital  |"<<endl;
-    cout<<" 3 | Netflix Premium Plan              | $23/month  | Digital  |"<<endl;
-    cout<<" 4 | Udemy Programming Course          | $15        | Digital  |"<<endl;
-    cout<<" 5 | Antivirus Software                | $30/year   | Digital  |"<<endl;
-    cout<<" 6 | Laptop                            | $800       | Physical |"<<endl;
-    cout<<" 7 | Smartphone                        | $600       | Physical |"<<endl;
-    cout<<" 8 | Wireless Mouse                    | $20        | Physical |"<<endl;
-    cout<<" 9 | Mechanical Keyboard               | $50        | Physical |"<<endl;
-    cout<<" 10| Headphones                        | $75        | Physical |"<<endl;
+class physicalProduct : public Products{
+    float weight;
+    public:
+    physicalProduct(int i , string n , float p , float w) : Products(i , n , p){
+        weight = w;
+    }
 
-    int n;
-    cout<<"Enter the number to select the product:"<<endl;
-    cin>>n;
-    switch(n)
-    {
-    case 1:
-    {
-        Product p("Microsoft Office 365 Subscription","$70/year ","Digital ");
-        break;
+    void show(){
+        cout<<"Weight : "<<weight<<endl;
     }
-    case 2:
-    {
-        Product p("Adobe Photoshop License","$23/month ","Digital ");
-        break;
+};
+
+class digitalProduct : public Products{
+    float fileSize;
+    public:
+    digitalProduct(int i , string n , float p , float f) : Products(i , n , p){
+        fileSize = f;
     }
-    case 3:
-    {
-        Product p("Netflix Premium Plan","$23/month ","Digital ");
-        break;
+
+    void show(){
+        cout<<"File Size : "<<fileSize<<endl;
     }
-    case 4:
-    {
-        Product p("Udemy Programming Course","$15","Digital ");
-        break;
-    }
-    default:
-        break;
-    }
-}
+};
